ceb7e4e8f54590b0efafb36d19a108532b53a566
aashishesr <aashish@esrtech.io>
2024-03-25 13:51:07 +0545

    ready fro eploy

--- apps/admin/app/features/Blogs/List/index.tsx
+++ apps/admin/app/features/Blogs/List/index.tsx
@@ -1,3 +1,4 @@
+'use client';
 import React, { useEffect, useState } from 'react';
 import { Button, Space, Table, notification, Popconfirm, Switch } from 'antd';
 import { ColumnsType } from 'antd/lib/table';
